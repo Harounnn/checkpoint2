b393b3eb639c32a9a34e880dcb5ac3b5661916ae
Harounnn <67702192+Harounnn@users.noreply.github.com>
2020-07-06 15:38:26 +0100

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+
+    float Sum(float a, float b)
+    {
+        return (a + b);
+    }
+    float Prod(float a , float b)
+    {
+        return (a * b);
+    }
+    float Div(float a , float b)
+    {
+        
+        if(b == 0)
+        {
+            Debug.Log("Eroor");
+        }
+    
+        return (a / b);
+    
+    }
+
+
+
+}
